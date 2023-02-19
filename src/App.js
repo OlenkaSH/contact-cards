f04b97f5a86aa5d384a7007480c17fd5e7232037
Olena Kraemer <olenakraem@gmail.com>
2023-02-19 03:48:17 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import logo from "./logo.svg";
 import "./App.css";
+import ContactCard from "./components/ContactCard";
 
 function App() {
   return (
     <div className="contacts">
-      <div className="contact-card">
-        <img src="http://placekitten.com/300/200" />
-        <h3>Mr. Whiskerson</h3>
-        <p>Phone: (030) 333-22-11</p>
-        <p>Email: mr.whiskazy@mail.meow</p>
-      </div>
-
-      <div className="contact-card">
-        <img src="http://placekitten.com/400/200" />
-        <h3>Fluffy</h3>
-        <p>Phone: (030) 543-22-11</p>
-        <p>Email: fluff@me.meow</p>
-      </div>
-
-      <div className="contact-card">
-        <img src="http://placekitten.com/400/300" />
-        <h3>Ms. Purrrson</h3>
-        <p>Phone: (030) 999-22-11</p>
-        <p>Email: purrrr@mail.meow</p>
-      </div>
-
-      <div className="contact-card">
-        <img src="http://placekitten.com/200/100" />
-        <h3>Felix</h3>
-        <p>Phone: (030) 876-22-11</p>
-        <p>Email: feliksss@mail.meow</p>
-      </div>
+      <ContactCard
+        contact={{
+          name: "Mr. Whiskerson",
+          imgUrl: "http://placekitten.com/300/200",
+          phone: "(030) 333-22-11",
+          email: "mr.whiskazy@mail.meow",
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: "Fluffy",
+          imgUrl: "http://placekitten.com/400/200",
+          phone: "(030) 543-22-11",
+          email: "fluff@me.meow",
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: "Ms. Purrrson",
+          imgUrl: "http://placekitten.com/400/300",
+          phone: "(030) 999-22-11",
+          email: "purrrr@mail.meow",
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: "Felix",
+          imgUrl: "http://placekitten.com/200/100",
+          phone: "(030) 876-22-11",
+          email: "feliksss@mail.meow",
+        }}
+      />
     </div>
   );
 }
---
+++ src/components/ContactCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ContactCard(props) {
+  //console.log(props);
+  return (
+    <div className="contact-card">
+      <img src={props.contact.imgUrl} />
+      <h3>{props.contact.name}</h3>
+      <p>Phone: {props.contact.phone}</p>
+      <p>Email: {props.contact.email}</p>
+    </div>
+  );
+}
